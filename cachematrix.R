188d39d1164e35e6c609bc366ac73d29eca1a0d1
MathiasBoehme <mb@m-g-b.de>
2020-06-17 11:33:40 +1000

    First edit, for a test

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,9 @@
+## This is Mathias's code for week 3's coding assignment
+## R Programming by Johns Hopkins University on Coursera
+
+## Thanks a lot for checking my code.
+## And best regards from Sydney, Australia
+
 ## Put comments here that give an overall description of what your
 ## functions do
 
